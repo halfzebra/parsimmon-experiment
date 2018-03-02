f152f8c218ccefb1d8d2959e0f37de54cd8eb069
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-03-02 01:04:55 +0100

    Implement module declaration parsing.

--- lib/statement.spec.ts
+++ lib/statement.spec.ts
@@ -3,7 +3,45 @@ import { moduleDeclaration } from './statement';
 describe('statement', () => {
   describe('moduleDeclaration', () => {
     it('should parse a module declaration', () => {
-      expect(moduleDeclaration.tryParse('module Main')).toBe('');
+      expect(() =>
+        moduleDeclaration.tryParse('module Main exposing (..)')
+      ).not.toThrow();
+    });
+
+    it('should parse a module with one function export', () => {
+      expect(() =>
+        moduleDeclaration.tryParse('module Main exposing (main)')
+      ).not.toThrow();
+    });
+
+    it('should parse a module declaration with multiple function exports', () => {
+      expect(() =>
+        moduleDeclaration.tryParse('module Main exposing (main, add, hello)')
+      ).not.toThrow();
+    });
+
+    it('should parse a module declaration with type export', () => {
+      expect(() =>
+        moduleDeclaration.tryParse('module Main exposing (Foo)')
+      ).not.toThrow();
+    });
+
+    it('should parse a module declaration with type export', () => {
+      expect(() =>
+        moduleDeclaration.tryParse('module Main exposing (Foo)')
+      ).not.toThrow();
+    });
+
+    it('should parse a module declaration with constructor export that exposes the whole set ', () => {
+      expect(() =>
+        moduleDeclaration.tryParse('module Main exposing (Foo(..))')
+      ).not.toThrow();
+    });
+
+    it('should parse a module declaration with constructor export that exposes only the subset ', () => {
+      expect(() =>
+        moduleDeclaration.tryParse('module Main exposing (Foo(Bar))')
+      ).not.toThrow();
     });
   });
 });
--- lib/statement.ts
+++ lib/statement.ts
@@ -1,5 +1,37 @@
 import * as Parsimmon from 'parsimmon';
 
-import { initialSymbol, moduleName } from './helpers';
+import {
+  initialSymbol,
+  moduleName,
+  symbol,
+  parens,
+  operator,
+  functionName,
+  commaSeparated,
+  upName,
+  spaces
+} from './helpers';
 
-export const moduleDeclaration = initialSymbol('module').then(moduleName);
+const allExport = symbol('..');
+
+const functionExport = Parsimmon.alt(functionName, parens(operator));
+
+const constructorSubsetExports = commaSeparated(upName);
+
+const constructorExports = parens(
+  allExport.or(constructorSubsetExports)
+).fallback('Nothing'); // FIXME: custom data-structure?
+
+const typeExport = Parsimmon.seq(
+  upName.trim(Parsimmon.optWhitespace),
+  constructorExports
+);
+
+const subsetExport = commaSeparated(Parsimmon.alt(functionExport, typeExport));
+
+const moduleExports = parens(Parsimmon.alt(allExport, subsetExport));
+
+export const moduleDeclaration = Parsimmon.seq(
+  initialSymbol('module').then(moduleName),
+  symbol('exposing').then(moduleExports)
+);
