46e27bf3f73e978d497d237e8d954eba58b94eb8
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-03-06 11:05:40 +0100

    Add a parser for parsing type annotations.

--- lib/statement.ts
+++ lib/statement.ts
@@ -8,10 +8,13 @@ import {
   operator,
   functionName,
   commaSeparated,
+  commaSeparated_,
   loName,
   upName,
-  spaces
+  spaces,
   braces,
+  spaces_,
+  newline
 } from './helpers';
 
 const allExport = symbol('..');
@@ -22,7 +25,7 @@ const constructorSubsetExports = commaSeparated(upName);
 
 const constructorExports = parens(
   allExport.or(constructorSubsetExports)
-).fallback('Nothing'); // FIXME: custom data-structure?
+).fallback('Nothing');
 
 const typeExport = Parsimmon.seq(
   upName.trim(Parsimmon.optWhitespace),
@@ -65,4 +68,89 @@ export const importStatement = Parsimmon.seq(
     .fallback('Nothing')
 );
 
-export const statement = Parsimmon.alt(moduleDeclaration, importStatement);
+// Type declarations.
+
+const typeVariable = Parsimmon.regex(/[a-z]+(\\w|_)*/);
+
+const typeConstant = upName.sepBy1(Parsimmon.string('.'));
+
+const typeApplication = symbol('->');
+
+const typeTuple = Parsimmon.lazy(() => parens(commaSeparated_(type_)));
+
+const typeRecordPair = Parsimmon.lazy(() =>
+  Parsimmon.seq(loName.skip(symbol(':')), typeAnnotation)
+);
+
+const typeRecordPairs = Parsimmon.lazy(() => commaSeparated_(typeRecordPair));
+
+const typeRecordConstructor = Parsimmon.lazy(() =>
+  braces(
+    Parsimmon.seq(
+      typeVariable.wrap(spaces, spaces),
+      symbol('|').then(typeRecordPairs)
+    )
+  )
+);
+
+const typeRecord = Parsimmon.lazy(() => braces(typeRecordPairs));
+
+const typeParameter = Parsimmon.lazy(() =>
+  Parsimmon.alt(
+    typeVariable,
+    typeConstant,
+    typeRecordConstructor,
+    typeRecord,
+    typeTuple,
+    parens(typeAnnotation)
+  ).wrap(
+    spaces.then(newline.then(spaces_).or(spaces)),
+    spaces.then(newline.then(spaces_).or(spaces))
+  )
+);
+
+const typeConstructor: Parsimmon.Parser<string> = Parsimmon.lazy(() =>
+  Parsimmon.seq(upName, typeParameter.many()).sepBy1(Parsimmon.string('.'))
+);
+
+const type_: Parsimmon.Parser<string> = Parsimmon.lazy(() =>
+  Parsimmon.alt(
+    typeConstructor,
+    typeVariable,
+    typeRecordConstructor,
+    typeRecord,
+    typeTuple,
+    parens(typeAnnotation)
+  ).wrap(spaces, spaces)
+);
+
+const typeAnnotation = Parsimmon.lazy(() => type_.sepBy(typeApplication));
+
+// Type declarations.
+
+export const typeAliasDeclaration = Parsimmon.lazy(() =>
+  Parsimmon.seq(
+    initialSymbol('type').then(symbol('alias').then(type_)),
+    Parsimmon.optWhitespace.then(symbol('=').then(typeAnnotation))
+  )
+).desc('typeAliasDeclaration');
+
+export const typeDeclaration = Parsimmon.seq(
+  initialSymbol('type').then(type_),
+  Parsimmon.optWhitespace.then(
+    symbol('=').then(
+      typeConstructor.trim(Parsimmon.optWhitespace).sepBy1(symbol('|'))
+    )
+  )
+);
+
+export const statement = Parsimmon.lazy(() =>
+  Parsimmon.alt(
+    moduleDeclaration,
+    portModuleDeclaration,
+    effectModuleDeclaration,
+    importStatement,
+    typeAliasDeclaration,
+    typeDeclaration
+  )
+);
