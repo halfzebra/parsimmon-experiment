d6f4dd1b3292bfbae56b08db66e6417b6c6e66b0
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-03-12 09:11:15 +0100

    Add missing types for Parsimmon.

---
+++ types/index.d.ts
@@ -0,0 +1,17 @@
+import { Index, Parser } from 'parsimmon';
+
+// TODO: send a PR to https://github.com/DefinitelyTyped/DefinitelyTyped/
+
+declare module 'parsimmon' {
+  interface Parser<T> {
+    wrap(left: Parser<any>, right: Parser<any>): Parser<any>;
+
+    sepBy1(separator: Parser<any>): Parser<T>;
+
+    sepBy(separator: Parser<any>): Parser<T>;
+
+    node(name?: string): Parser<Index>;
+
+    tie(): Parser<string>;
+  }
+}
