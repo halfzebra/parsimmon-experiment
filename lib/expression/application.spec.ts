d789f3941f2c10eccc3f1faee56e3d94e9b00a79
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-07-15 23:37:35 +0200

    Implement the application parser.

--- lib/expression/application.spec.ts
+++ lib/expression/application.spec.ts
@@ -1,16 +1,30 @@
-import { application } from './application';
+import { application, spacesOrIndentedNewline } from './application';
 import { operators } from '../binOp';
 
 describe('function application', () => {
-  it.skip('should parse simple application', () => {
-    expect(application(operators).tryParse('f a')).toEqual(false);
+  it('should parse simple application', () => {
+    expect(application(operators).parse('f a').status).toEqual(true);
   });
 
-  it.skip('should parse curried application', () => {
-    expect(application(operators).parse('f a b')).toEqual(false);
+  it('should parse curried application', () => {
+    expect(application(operators).parse('f a b').status).toEqual(true);
   });
 
-  it.skip('should parse curried application', () => {
-    expect(application(operators).parse('(f a) b')).toEqual(false);
+  it.skip('should parse curried application with parens', () => {
+    expect(application(operators).parse('(f a) b')).toEqual(true);
+  });
+});
+
+describe('spacesOrIndentedNewline', () => {
+  it('should not fail if the column is at the start', () => {
+    expect(spacesOrIndentedNewline(0).tryParse('\n\n\n\n\n ')).toBe('');
+  });
+
+  it('should fail if there is no indentation', () => {
+    expect(() => spacesOrIndentedNewline(1).tryParse('')).toThrow('');
+  });
+
+  it('should count only spaces', () => {
+    expect(spacesOrIndentedNewline(3).tryParse('\n    ')).toBe('');
   });
 });
--- lib/expression/application.ts
+++ lib/expression/application.ts
@@ -1,5 +1,35 @@
 import * as Parsimmon from 'parsimmon';
 import { OpTable } from '../binOp';
 import { term } from '../expression';
+import { chainl, countIndent, spaces1, whitespace } from '../helpers';
 
-export const application = (ops: OpTable) => Parsimmon.lazy(() => term(ops));
+export const spacesOrIndentedNewline = (indentation: number) =>
+  Parsimmon.alt(
+    spaces1,
+    countIndent
+      .chain(column => {
+        // console.log(`column < indentation ${column} < ${indentation}`);
+        if (column < indentation) {
+          return Parsimmon.fail(
+            'Arguments have to be at least the same indentation as the function'
+          );
+        }
+        return whitespace;
+      })
+      .desc(`${indentation} spaces`)
+  );
+
+const withColumn = (fn: (value: any) => Parsimmon.Parser<any>) =>
+  Parsimmon.index.map(({ column }) => column).chain(fn);
+
+const applicationNode = (a: any, b: any) => ({
+  name: 'application',
+  value: [a, b]
+});
+
+export const application = (ops: OpTable) =>
+  Parsimmon.lazy(() =>
+    withColumn((column: number) =>
+      chainl(spacesOrIndentedNewline(column - 1).map(() => applicationNode), term(ops))
+    )
+  ).node('application');
