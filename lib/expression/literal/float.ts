ef625856cbac5c18e4bf599420f910a6444546f5
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-07-15 23:35:47 +0200

    Add the description for float.

--- lib/expression/literal/float.ts
+++ lib/expression/literal/float.ts
@@ -5,4 +5,4 @@ export const float = Parsimmon.seqMap(
   sign,
   Parsimmon.regex(/(0|[1-9][0-9]*)(\.[0-9]+)/).map(parseFloat),
   (parsedSign: number = 1, parsed: number) => parsedSign * parsed
-);
+).desc('float');
