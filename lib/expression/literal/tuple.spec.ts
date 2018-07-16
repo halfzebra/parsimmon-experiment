2f151eed43f1d3e92a3a264a817a9fa2818b5bf3
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-07-17 00:16:36 +0200

    Fix the simple Tuple test.

--- lib/expression/literal/tuple.spec.ts
+++ lib/expression/literal/tuple.spec.ts
@@ -5,14 +5,17 @@ import { parseExpression } from '../../ast';
 describe('tuples', () => {
   it('can parse empty tuple', () => {
     expect(parseExpression(operators).parse('()').status).toBe(true);
+    expect(parseExpression(operators).parse('()').value.value.value.name).toBe(
+      'Tuple'
+    );
   });
 
   it('can parse empty tuple with spaces inside', () => {
-    expect(parseExpression(operators).tryParse('(   )')).toEqual([]);
+    expect(parseExpression(operators).parse('(   )').status).toEqual(true);
   });
 
   it.skip('can parse simple tuple', () => {
-    expect(parseExpression(operators).parse('(a, b)').status).toBe(true);
+    expect(parseExpression(operators).tryParse('(a, b)')).toBe(true);
   });
 
   it.skip('can parse simple tuple with format', () => {
